063fce1a67fbae60d0b935b76e4bb7834093bc32
Dinh-Van Colomban <van.colomban@gmail.com>
2024-06-13 21:20:43 +0200

    fix(build): adds exclusion for .spec file in tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['lib', '!lib/**/*.test.ts'],
+  entry: ['lib', '!lib/**/*.test.ts', '!lib/**/*.spec.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
