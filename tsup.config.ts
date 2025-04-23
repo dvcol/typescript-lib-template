8da2b5edd2904965d72255505253128be6f6a5bd
Dinh-Van Colomban <van.colomban@gmail.com>
2025-04-23 18:17:38 +0200

    feat(tsconfig): rework tsconfig setup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['lib', '!lib/**/*.test.ts', '!lib/**/*.spec.ts'],
+  entry: ['lib', '!lib/**/*.test.ts', '!lib/**/*.spec.ts', '!lib/mocks'],
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
   splitting: true,
   sourcemap: false,
-  tsconfig: 'tsconfig.lib.json',
 });
